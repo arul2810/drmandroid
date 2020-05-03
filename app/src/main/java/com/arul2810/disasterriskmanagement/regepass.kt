f9be85121b3e0db6c314233f86801b6fd2e8b90d
Arul Prakash <arul2810@gmail.com>
2020-05-04 00:10:23 +0530

    Image View on Register E Pass

--- app/src/main/java/com/arul2810/disasterriskmanagement/regepass.kt
+++ app/src/main/java/com/arul2810/disasterriskmanagement/regepass.kt
@@ -2,11 +2,16 @@ package com.arul2810.disasterriskmanagement
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_regepass.*
+import java.util.*
 
 class regepass : AppCompatActivity() {
 
@@ -14,37 +19,61 @@ class regepass : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regepass)
 
-         val PICK_IMAGE_REQUEST = 71
-         //var filePath: Uri? = null
-         //var firebaseStore: FirebaseStorage? = null
-         //var storageReference: StorageReference? = null
+        val PICK_IMAGE_REQUEST = 71
+        //var filePath: Uri? = null
+        //var firebaseStore: FirebaseStorage? = null
+        //var storageReference: StorageReference? = null
 
-
-            fun applyprocess() {
+        uploadproof.setOnClickListener {
+            val proofuploadimage = Intent(Intent.ACTION_PICK)
+            proofuploadimage.type = "image/*"
+            startActivityForResult(proofuploadimage, 0)
+        }
+         var selectedPhotoUri: Uri? = null
+        applyepass.setOnClickListener {
             val Name = name.text.toString()
             val Aadhaar = aadhaar.text.toString()
             val Purpose = purpose.text.toString()
 
             if (Name.isEmpty() || Aadhaar.isEmpty() || Purpose.isEmpty()) {
                 Toast.makeText(this, "Please Fill All The Fields", Toast.LENGTH_SHORT).show()
-                return
+            } else {
+
+                val filename = UUID.randomUUID().toString()
+                val reef = FirebaseStorage.getInstance("/images/$filename")
+                reef.putFile(selectedPhotoUri)
+
+
             }
 
-                uploadproof.setOnClickListener {
-                val proofuploadimage = Intent()
-                    proofuploadimage.type = "image/*"
-                    proofuploadimage.action = Intent.ACTION_GET_CONTENT
-                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
-                }
+        }
+    }
 
-        applyepass.setOnClickListener {
-            applyprocess()
+
+         override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+
+             super.onActivityResult(requestCode, resultCode, data)
+
+            if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null)
+            {
+                //code to check the image
+                val uri = data.data
+                uploadview.setImageURI(uri)
+            }
         }
 
 
-        }
+
+
+
+
+
+
+
+
+
+
 
     }
-}
 
 
